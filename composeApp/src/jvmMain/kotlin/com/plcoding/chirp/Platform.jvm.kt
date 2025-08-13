f0450a48b7c0be6c86feef20713fb1758bd3bebd
Philipp Lackner <mail@pl-coding.com>
2025-08-13 11:57:00 +0200

    Create CmpApplicationConventionPlugin

--- composeApp/src/jvmMain/kotlin/com/plcoding/chirp/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package com.plcoding.chirp
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
