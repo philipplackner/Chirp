a2ace88f2db951b0531f79f6b1fc788a2bddee6e
Philipp Lackner <mail@pl-coding.com>
2025-08-14 21:13:40 +0200

    Implement Chirp password text field

--- core/designsystem/src/commonMain/kotlin/com/plcoding/core/designsystem/components/textfields/ChirpTextField.kt
+++ core/designsystem/src/commonMain/kotlin/com/plcoding/core/designsystem/components/textfields/ChirpTextField.kt
@@ -46,27 +46,14 @@ fun ChirpTextField(
     keyboardType: KeyboardType = KeyboardType.Text,
     onFocusChanged: (Boolean) -> Unit = {},
 ) {
-    val interactionSource = remember {
-        MutableInteractionSource()
-    }
-    val isFocused by interactionSource.collectIsFocusedAsState()
-
-    LaunchedEffect(isFocused) {
-        onFocusChanged(isFocused)
-    }
-
-    Column(
+    ChirpTextFieldLayout(
+        title = title,
+        isError = isError,
+        supportingText = supportingText,
+        enabled = enabled,
+        onFocusChanged = onFocusChanged,
         modifier = modifier
-    ) {
-        if(title != null) {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.labelSmall,
-                color = MaterialTheme.colorScheme.extended.textSecondary
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-        }
-
+    ) { styleModifier, interactionSource ->
         BasicTextField(
             state = state,
             enabled = enabled,
@@ -85,28 +72,7 @@ fun ChirpTextField(
             ),
             cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface),
             interactionSource = interactionSource,
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color = when {
-                        isFocused -> MaterialTheme.colorScheme.primary.copy(
-                            alpha = 0.05f
-                        )
-                        enabled -> MaterialTheme.colorScheme.surface
-                        else -> MaterialTheme.colorScheme.extended.secondaryFill
-                    },
-                    shape = RoundedCornerShape(8.dp)
-                )
-                .border(
-                    width = 1.dp,
-                    color = when {
-                        isError -> MaterialTheme.colorScheme.error
-                        isFocused -> MaterialTheme.colorScheme.primary
-                        else -> MaterialTheme.colorScheme.outline
-                    },
-                    shape = RoundedCornerShape(8.dp)
-                )
-                .padding(12.dp),
+            modifier = styleModifier,
             decorator = { innerBox ->
                 Box(
                     modifier = Modifier
@@ -124,19 +90,6 @@ fun ChirpTextField(
                 }
             }
         )
-
-        if(supportingText != null) {
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = supportingText,
-                color = if(isError) {
-                    MaterialTheme.colorScheme.error
-                } else {
-                    MaterialTheme.colorScheme.extended.textTertiary
-                },
-                style = MaterialTheme.typography.bodySmall
-            )
-        }
     }
 }
 
