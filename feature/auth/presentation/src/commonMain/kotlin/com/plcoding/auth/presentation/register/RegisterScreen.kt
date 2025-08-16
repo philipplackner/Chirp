4bf08f8b9e2fed5cd7a6a6ad0a2768a3c26071ed
Philipp Lackner <mail@pl-coding.com>
2025-08-16 18:42:50 +0200

    Registering in RegisterViewModel

--- feature/auth/presentation/src/commonMain/kotlin/com/plcoding/auth/presentation/register/RegisterScreen.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/plcoding/auth/presentation/register/RegisterScreen.kt
@@ -30,16 +30,25 @@ import com.plcoding.core.designsystem.components.layouts.ChirpSnackbarScaffold
 import com.plcoding.core.designsystem.components.textfields.ChirpPasswordTextField
 import com.plcoding.core.designsystem.components.textfields.ChirpTextField
 import com.plcoding.core.designsystem.theme.ChirpTheme
+import com.plcoding.core.presentation.util.ObserveAsEvents
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun RegisterRoot(
-    viewModel: RegisterViewModel = viewModel()
+    viewModel: RegisterViewModel = viewModel(),
+    onRegisterSuccess: (String) -> Unit,
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
-
     val snackbarHostState = remember { SnackbarHostState() }
+
+    ObserveAsEvents(viewModel.events) { event ->
+        when(event) {
+            is RegisterEvent.Success -> {
+                onRegisterSuccess(event.email)
+            }
+        }
+    }
 
     RegisterScreen(
         state = state,
