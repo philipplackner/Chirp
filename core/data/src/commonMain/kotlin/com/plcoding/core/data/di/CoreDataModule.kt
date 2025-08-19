bdeb101a03bc0d19e765e14a4c815f62580d1867
Philipp Lackner <mail@pl-coding.com>
2025-08-19 17:15:20 +0200

    Implement DataStore session storage

--- core/data/src/commonMain/kotlin/com/plcoding/core/data/di/CoreDataModule.kt
+++ core/data/src/commonMain/kotlin/com/plcoding/core/data/di/CoreDataModule.kt
@@ -1,9 +1,11 @@
 package com.plcoding.core.data.di
 
+import com.plcoding.core.data.auth.DataStoreSessionStorage
 import com.plcoding.core.data.auth.KtorAuthService
 import com.plcoding.core.data.logging.KermitLogger
 import com.plcoding.core.data.networking.HttpClientFactory
 import com.plcoding.core.domain.auth.AuthService
+import com.plcoding.core.domain.auth.SessionStorage
 import com.plcoding.core.domain.logging.ChirpLogger
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
@@ -19,4 +21,5 @@ val coreDataModule = module {
         HttpClientFactory(get()).create(get())
     }
     singleOf(::KtorAuthService) bind AuthService::class
+    singleOf(::DataStoreSessionStorage) bind SessionStorage::class
 }
--- feature/auth/presentation/src/commonMain/kotlin/com/plcoding/auth/presentation/login/LoginViewModel.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/plcoding/auth/presentation/login/LoginViewModel.kt
@@ -7,6 +7,7 @@ import chirp.feature.auth.presentation.generated.resources.Res
 import chirp.feature.auth.presentation.generated.resources.error_email_not_verified
 import chirp.feature.auth.presentation.generated.resources.error_invalid_credentials
 import com.plcoding.core.domain.auth.AuthService
+import com.plcoding.core.domain.auth.SessionStorage
 import com.plcoding.core.domain.util.DataError
 import com.plcoding.core.domain.util.onFailure
 import com.plcoding.core.domain.util.onSuccess
@@ -27,7 +28,8 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class LoginViewModel(
-    private val authService: AuthService
+    private val authService: AuthService,
+    private val sessionStorage: SessionStorage
 ) : ViewModel() {
 
     private var hasLoadedInitialData = false
@@ -104,6 +106,8 @@ class LoginViewModel(
                     password = password
                 )
                 .onSuccess { authInfo ->
+                    sessionStorage.set(authInfo)
+
                     _state.update { it.copy(
                         isLoggingIn = false
                     ) }
