98e1cf079a23e1355e80a20f086beb1e746c1f0e
Philipp Lackner <mail@pl-coding.com>
2025-08-20 12:40:13 +0200

    Implement forgot password functionality

--- feature/auth/presentation/src/commonMain/kotlin/com/plcoding/auth/presentation/forgot_password/ForgotPasswordState.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/plcoding/auth/presentation/forgot_password/ForgotPasswordState.kt
@@ -5,7 +5,6 @@ import com.plcoding.core.presentation.util.UiText
 
 data class ForgotPasswordState(
     val emailTextFieldState: TextFieldState = TextFieldState(),
-    val emailError: UiText? = null,
     val canSubmit: Boolean = false,
     val isLoading: Boolean = false,
     val errorText: UiText? = null,
