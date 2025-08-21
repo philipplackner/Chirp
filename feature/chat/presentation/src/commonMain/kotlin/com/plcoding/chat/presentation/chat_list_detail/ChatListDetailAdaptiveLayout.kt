343e5520fbb1523022127b7394b8d255ea550a63
Philipp Lackner <mail@pl-coding.com>
2025-08-21 13:29:24 +0200

    Implement create chat dialog UI

--- feature/chat/presentation/src/commonMain/kotlin/com/plcoding/chat/presentation/chat_list_detail/ChatListDetailAdaptiveLayout.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/plcoding/chat/presentation/chat_list_detail/ChatListDetailAdaptiveLayout.kt
@@ -27,7 +27,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.backhandler.BackHandler
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.plcoding.chat.presentation.create_chat.CreateChatRoot
 import com.plcoding.core.designsystem.theme.extended
+import com.plcoding.core.presentation.util.DialogSheetScopedViewModel
 import kotlinx.coroutines.launch
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -64,6 +66,7 @@ fun ChatListDetailAdaptiveLayout(
                             text = "Chat $chatIndex",
                             modifier = Modifier
                                 .clickable {
+                                    chatListDetailViewModel.onAction(ChatListDetailAction.OnCreateChatClick)
                                     chatListDetailViewModel.onAction(ChatListDetailAction.OnChatClick(chatIndex.toString()))
                                     scope.launch {
                                         scaffoldNavigator.navigateTo(
@@ -93,4 +96,10 @@ fun ChatListDetailAdaptiveLayout(
             }
         }
     )
+
+    DialogSheetScopedViewModel(
+        visible = sharedState.dialogState is DialogState.CreateChat
+    ) {
+        CreateChatRoot()
+    }
 }
---
+++ feature/chat/presentation/src/commonMain/kotlin/com/plcoding/chat/presentation/create_chat/CreateChatState.kt
@@ -0,0 +1,16 @@
+package com.plcoding.chat.presentation.create_chat
+
+import androidx.compose.foundation.text.input.TextFieldState
+import com.plcoding.core.designsystem.components.avatar.ChatParticipantUi
+import com.plcoding.core.presentation.util.UiText
+
+data class CreateChatState(
+    val queryTextState: TextFieldState = TextFieldState(),
+    val selectedChatParticipants: List<ChatParticipantUi> = emptyList(),
+    val isAddingParticipant: Boolean = false,
+    val isLoadingParticipants: Boolean = false,
+    val canAddParticipant: Boolean = false,
+    val currentSearchResult: ChatParticipantUi? = null,
+    val searchError: UiText? = null,
+    val isCreatingChat: Boolean = false,
+)
