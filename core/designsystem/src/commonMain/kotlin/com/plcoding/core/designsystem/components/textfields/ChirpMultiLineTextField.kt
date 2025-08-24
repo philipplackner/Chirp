0a2024f323669d9caffa366b4869d1f6768699e6
Philipp Lackner <mail@pl-coding.com>
2025-08-24 12:13:24 +0200

    Implement message box UI

--- core/designsystem/src/commonMain/kotlin/com/plcoding/core/designsystem/components/textfields/ChirpMultiLineTextField.kt
+++ core/designsystem/src/commonMain/kotlin/com/plcoding/core/designsystem/components/textfields/ChirpMultiLineTextField.kt
@@ -70,7 +70,7 @@ fun ChirpMultiLineTextField(
             },
             cursorBrush = SolidColor(MaterialTheme.colorScheme.extended.textPrimary),
             decorator = { innerBox ->
-                if(placeholder != null && state.text.isNotEmpty()) {
+                if(placeholder != null && state.text.isEmpty()) {
                     Text(
                         text = placeholder,
                         color = MaterialTheme.colorScheme.extended.textPlaceholder,
