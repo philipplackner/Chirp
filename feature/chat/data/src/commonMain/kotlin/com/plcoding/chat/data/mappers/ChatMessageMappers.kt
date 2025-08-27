9a7b94619fffadfc341ce9e5f6469c6300718868
Philipp Lackner <mail@pl-coding.com>
2025-08-27 11:51:41 +0200

    Parse and handle incoming web socket messages

--- feature/chat/data/src/commonMain/kotlin/com/plcoding/chat/data/mappers/ChatMessageMappers.kt
+++ feature/chat/data/src/commonMain/kotlin/com/plcoding/chat/data/mappers/ChatMessageMappers.kt
@@ -1,6 +1,7 @@
 package com.plcoding.chat.data.mappers
 
 import com.plcoding.chat.data.dto.ChatMessageDto
+import com.plcoding.chat.data.dto.websocket.IncomingWebSocketDto
 import com.plcoding.chat.data.dto.websocket.OutgoingWebSocketDto
 import com.plcoding.chat.database.entities.ChatMessageEntity
 import com.plcoding.chat.database.view.LastMessageView
@@ -68,5 +69,16 @@ fun ChatMessage.toNewMessage(): OutgoingWebSocketDto.NewMessage {
         messageId = id,
         chatId = chatId,
         content = content,
+    )
+}
+
+fun IncomingWebSocketDto.NewMessageDto.toEntity(): ChatMessageEntity {
+    return ChatMessageEntity(
+        messageId = id,
+        chatId = chatId,
+        senderId = senderId,
+        content = content,
+        timestamp = Instant.parse(createdAt).toEpochMilliseconds(),
+        deliveryStatus = ChatMessageDeliveryStatus.SENT.name
     )
 }
