60091d3a9411e70b8b9da44c6749c4d5e8affb2f
Philipp Lackner <mail@pl-coding.com>
2025-08-29 11:13:35 +0200

    Group messages by date

--- feature/chat/presentation/src/commonMain/kotlin/com/plcoding/chat/presentation/mappers/ChatMessageMappers.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/plcoding/chat/presentation/mappers/ChatMessageMappers.kt
@@ -3,11 +3,21 @@ package com.plcoding.chat.presentation.mappers
 import com.plcoding.chat.domain.models.MessageWithSender
 import com.plcoding.chat.presentation.model.MessageUi
 import com.plcoding.chat.presentation.util.DateUtils
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 fun List<MessageWithSender>.toUiList(localUserId: String): List<MessageUi> {
     return this
         .sortedByDescending { it.message.createdAt }
-        .map { it.toUi(localUserId) }
+        .groupBy {
+            it.message.createdAt.toLocalDateTime(TimeZone.currentSystemDefault()).date
+        }
+        .flatMap { (date, messages) ->
+            messages.map { it.toUi(localUserId) } + MessageUi.DateSeparator(
+                id = date.toString(),
+                date = DateUtils.formatDateSeparator(date)
+            )
+        }
 }
 
 fun MessageWithSender.toUi(
