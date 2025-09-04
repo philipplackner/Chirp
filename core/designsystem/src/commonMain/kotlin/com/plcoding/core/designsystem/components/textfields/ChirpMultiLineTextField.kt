7f37363f2044c330469b0af2612288a85eeddedc
Philipp Lackner <mail@pl-coding.com>
2025-09-04 13:01:37 +0200

    Give message field proper IME padding and request focus when tapping inside

--- core/designsystem/src/commonMain/kotlin/com/plcoding/core/designsystem/components/textfields/ChirpMultiLineTextField.kt
+++ core/designsystem/src/commonMain/kotlin/com/plcoding/core/designsystem/components/textfields/ChirpMultiLineTextField.kt
@@ -2,6 +2,7 @@ package com.plcoding.core.designsystem.components.textfields
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,8 +22,11 @@ import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.unit.dp
 import com.plcoding.core.designsystem.components.buttons.ChirpButton
@@ -41,6 +45,9 @@ fun ChirpMultiLineTextField(
     maxHeightInLines: Int = 3,
     bottomContent: @Composable (RowScope.() -> Unit)? = null
 ) {
+    val textFieldFocusRequester = remember {
+        FocusRequester()
+    }
     Column(
         modifier = modifier
             .background(
@@ -52,6 +59,13 @@ fun ChirpMultiLineTextField(
                 color = MaterialTheme.colorScheme.extended.surfaceOutline,
                 shape = RoundedCornerShape(16.dp)
             )
+            .clickable(
+                interactionSource = null,
+                indication = null,
+                onClick = {
+                    textFieldFocusRequester.requestFocus()
+                }
+            )
             .padding(
                 vertical = 12.dp,
                 horizontal = 16.dp
@@ -61,6 +75,9 @@ fun ChirpMultiLineTextField(
         BasicTextField(
             state = state,
             enabled = enabled,
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(textFieldFocusRequester),
             textStyle = MaterialTheme.typography.bodyLarge.copy(
                 color = MaterialTheme.colorScheme.extended.textPrimary
             ),
